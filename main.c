f4f3adaa9ccd32f5a1c74d4f5c8f774fe467411c
Jehanzeb Mirza <jehanzeb@ualberta.ca>
2022-04-01 16:18:00 -0600

    Updated code, Now developing an understanding of GLib io channels.

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
-#include <stdio.h>
 #include <glib.h>
+#include <glib/gprintf.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main()
-{
-    GList* list = NULL;
-    list = g_list_append(list, "Hello world!");
-    printf("The first item in the list is '%s'\n", g_list_first(list)->data);
-    return 0;
+
+gboolean stdin_callback(GIOChannel *source, GIOCondition condition,
+                        gpointer data) {
+  gchar *in_str;
+  gsize strlen;
+  gsize terminator_pos;
+  GError *errs;
+
+  if (g_io_channel_read_line(source, &in_str, &strlen, &terminator_pos, NULL) !=
+      G_IO_STATUS_NORMAL)
+    return FALSE;
+
+  g_printf("SIGNAL: STDIN READ %zu bytes\n", strlen);
+  g_free(in_str);
+  return TRUE;
+}
+
+
+int main() {
+  GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+  GIOChannel *stdin_channel = g_io_channel_unix_new(STDIN_FILENO);
+
+  g_io_add_watch(stdin_channel, G_IO_IN, stdin_callback, NULL);
+
+  g_main_loop_run(loop);
+  return 0;
 }
