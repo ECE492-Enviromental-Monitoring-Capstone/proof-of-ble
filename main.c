4e68e707f81277408c42b6593d7f7319f1dba47b
Jehanzeb Mirza <jehanzeb@ualberta.ca>
2022-03-31 20:18:23 -0600

    Updated a few random here and there files.
    It's not a serious commit yet. Developement enviroment changed to good
    old notepad++.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <glib.h>
 
 int main()
 {
-    printf("Hello World\n");
+    GList* list = NULL;
+    list = g_list_append(list, "Hello world!");
+    printf("The first item in the list is '%s'\n", g_list_first(list)->data);
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
 EXE=a.out
+CFLAGS=-I/usr/include/glib-2.0 \
+	-I/usr/lib/arm-linux-gnueabihf/glib-2.0/include
+LIBS=-lglib-2.0
 
 ${EXE}: main.c
-	cc main.c -o ${EXE}
+	cc main.c $(CFLAGS) $(LIBS) -o ${EXE}
 
 .PHONY: run clean
 
